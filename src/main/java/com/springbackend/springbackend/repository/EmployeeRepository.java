3434f6725593b0ad1ca15856813ca75cf9149115
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2023-02-10 09:03:41 +0530

    Exception handling created

---
+++ src/main/java/com/springbackend/springbackend/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.springbackend.springbackend.repository;
+
+import com.springbackend.springbackend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
